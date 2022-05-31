f45ac9b26634b000d3dc69607128722bb2b062b7
EgeniiM <106548205+EgeniiM@users.noreply.github.com>
2022-05-31 08:46:58 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        byte money = 155;
+        sbyte speedIndex = -10;
+
+        short undestandLevel = -100;
+        ushort height = 100;
+
+        int skillLevel = -1;
+        uint mana = 24;
+
+        long treasureGold = -12325432624564536;
+        ulong allElements = 1234234243;
+
+        float shipSpeed = 1f;
+
+        string name = "Egenii";
+        
+
+        
+    }
+}
