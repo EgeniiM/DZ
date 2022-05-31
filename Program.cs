3b59e129916e71fb12e2dda2b42fc6c561f9c726
EgeniiM <106548205+EgeniiM@users.noreply.github.com>
2022-05-31 17:24:56 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,29 +1,19 @@
-﻿using System;
-
-
-    class Program
+using System;
+class Program
 {
     static void Main(string[] args)
     {
-
-
         byte money = 155;
         sbyte speedIndex = -10;
-
         short undestandLevel = -100;
         ushort height = 100;
-
         int skillLevel = -1;
         uint mana = 24;
-
         long treasureGold = -12325432624564536;
         ulong allElements = 1234234243;
-
         float shipSpeed = 1f;
-
         string name = "Egenii";
-        
-
-        
+        bool isСharged = false;
+        bool hasKey = true;
     }
 }
